2e8824fdc3436783740628d658b938e63c445fae
Georgi-Progger <hugaeag@gmail.com>
2023-01-09 18:31:12 +0300

    first commit

---
+++ ProdCalculate/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProdCalculate.Models;
+using System.Diagnostics;
+
+namespace ProdCalculate.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult CalculateFactorial(int n)
+        {
+            int c = 0;
+            c = n * (n + 1) * (2 * n + 1) / 6;
+            TaskModel taskModel = new TaskModel { IntN = c };
+            return View(taskModel);
+        }
+
+        public IActionResult DateCalculate(string date1, string date2)
+        {
+            DateTime startTime, endTime;
+            startTime = Convert.ToDateTime(date1);
+            endTime = Convert.ToDateTime(date2);
+            int t = (startTime.Second - endTime.Second) + (startTime.Minute - endTime.Minute) * 60 + (startTime.Hour - endTime.Hour)* 3600;
+            TaskModel taskModel = new TaskModel { Time = t };
+            return View(taskModel);
+        }
+    }
+}
---
+++ ProdCalculate/Models/TaskModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProdCalculate.Models
+{
+    public class TaskModel
+    {
+        public int IntN { get; set; }
+        public int Time { get; set; }
+    }
+}
